d61c54d3189fe9d839f440a221a42efa15554524
Hsuhui <la10120818@yahoo.com.tw>
2021-09-23 15:15:51 +0800

    2021/09/23
    update

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,37 @@
     </div>
     <p>v-pre這個指令會讓指定元素被忽略</p>
     <h2 v-pre>{{ preTitle }}</h2>
+    <!--v-cloak指令只會存在編譯過程中-->
+    <div v-cloak>${ item.title }</div>
+    <!--v-html直接把html標籤綁入頁面中
+    只能對可信任的內容使用v-html，絕不可對「用戶」提供的內容使用v-html，
+    不然很容易中「XSS攻擊」
+    -->
+    <div v-html="ithelp"></div>
+    <div>{{youtube}}</div>
+    <div class="main">
+      <button @click="change = !change">縮放控制器</button>
+      <transition name="zoom">
+        <div v-if="change" class="ant_man_style"></div>
+      </transition>
+    </div>
+    <div>空汙指數 {{ airPollution | filterData | releaseTime(saveDate) }}</div>
+    <h1>父層藏好料</h1>
+    <child @hungry="hungry" :dinner="dinner" />
   </div>
 </template>
 <script>
 import Breadcrumb from './components/Breadcrumb'
+import child from './components/child'
+import * as dayjs from 'dayjs'
+import * as axios from 'axios'
+
+
+const saveDate = dayjs().format('YYYY-MM-DD')
+var url='http://localhost:8081/data.json'
+axios.get(url)
+    .then( (response) => console.log(response))
+    .catch( (error) => console.log(error))
 
 export default {
   name:'about',
@@ -36,7 +63,13 @@ export default {
       myData: {
         myName: 'Adam',
         mail: 'Adam@xxx.com.tw'
-      }
+      },
+      ithelp: '<a target="_black" href="https://ithelp.ithome.com.tw/">it邦幫忙</a>',
+      youtube: '<a target="_black" href="https://www.youtube.com/?gl=TW">聽音樂去！</a>',
+      change: true,
+      airPollution: 152,
+      saveDate: saveDate,
+      dinner:[],
     }
   },
   watch: {
@@ -55,14 +88,36 @@ export default {
       deep: true
     }
   },
-  components() {
+  components: {
+    child,
     Breadcrumb
+    
   },
+  methods:{
+    hungry(){
+      alert('晚餐到了!')
+      return this.dinner=['漢堡','薯條','炸雞','可樂']
+    }
+  },
+  filters: {
+      filterData(airPollution) {
+        return 'AQI：' + airPollution + ' / 時間：'
+    },
+      releaseTime(saveDate, airPollution) {
+        return saveDate + airPollution
+    }
+  }
 }
 </script>
->
+
 <style lang="scss">
 #app {
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  flex-direction: column;
+  -webkit-align-items: center;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -82,4 +137,25 @@ export default {
     }
   }
 }
+.main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  button {
+    margin-bottom: 20px;
+  }
+  .ant_man_style {
+    background: url(assets/ant_man.png) no-repeat center center;
+    background-size: contain;
+    width: 200px;
+    height: 200px;
+  }
+  .zoom-enter, .zoom-leave-to {
+    width: 0px;
+    height: 0px;
+  }
+  .zoom-enter-active, .zoom-leave-active {
+    transition: width 1s, height 1s;
+  }
 </style>
